988ef3b91538521201c466a8f3bb7fd23811c288
Laxmi Sharma <laxminla@yahoo.com>
2021-08-05 14:17:48 -0700

    added lon and lat

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 
-// function getWeather() {
-//     const key = '4a18b5da63049b1fd79c9ec12ae13b97'
-//     const url = 'api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}'
-
-    // fetch(url).then(resp => resp.json()).then(data => console.log(data))
-
-
-
 const apiKey = '4a18b5da63049b1fd79c9ec12ae13b97'
-var searchTerm = document.querySelector('#cityName').value;
-var url = "http://www.openweather.com/weather?q=" + searchTerm + '&api=' + apiKey;
+var url = 'https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=4a18b5da63049b1fd79c9ec12ae13b97'
+// "http://www.openweather.com/weather?q=" + searchTerm + '&api=' + apiKey;
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=sacramento&appid=4a18b5da63049b1fd79c9ec12ae13b97')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
 
 
-var lat = 38.4666
-var lon = -121.3177
-var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=4a18b5da63049b1fd79c9ec12ae13b97'
+function displayWeather() {
+  var lat;
+  var lon;
+  var searchTerm = document.querySelector('#cityName').value;
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey;
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      lat = data["coord"]["lat"];
+      lon = data['coord']["lon"];
+      console.log(lat, lon)
+      var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=' + apiKey;
+      console.log(oneCallUrl, lat, lon)
+
+      fetch(oneCallUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          
+
+        });
+    });
 
 
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=4a18b5da63049b1fd79c9ec12ae13b97')
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+
+}
+
+
+
+
+
+
+  // moment().format("MMM Do YY");
+  // var today = moment();
